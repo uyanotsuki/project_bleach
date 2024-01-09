412a39f1c7ba87250c3f249217f9eecc9304ee8e
whoyoshi <diana.azamatovva@gmail.com>
2024-01-09 23:17:52 +0300

    10.7 функционал logout

--- bleach/routes/index.js
+++ bleach/routes/index.js
@@ -47,6 +47,13 @@ router.get('/logreg', function(req, res, next) {
   res.render('logreg',{error:null});
 });
    
+/* POST logout. */
+router.post('/logout', function(req, res, next) {
+  req.session.destroy();
+  res.locals.user = null;
+  res.redirect('/');
+});
+
 module.exports = router;
 
 
