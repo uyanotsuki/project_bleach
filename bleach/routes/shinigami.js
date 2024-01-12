b4befa747e51279a839a2e3a2ac430afefac4e36
kageyoshi <diana.azamatovva@gmail.com>
2024-01-12 20:35:48 +0300

    11.3 вывод данных на экран

--- bleach/routes/shinigami.js
+++ bleach/routes/shinigami.js
@@ -1,32 +1,51 @@
 const express = require('express');
 const router = express.Router();
-const Bleach = require("../models/bleach").Bleach;
+var db = require('../mySQLConnect.js');
+// const Bleach = require("../models/bleach").Bleach;
 const async = require("async");
-var checkAuth = require("./../middleware/checkAuth.js");
+// var checkAuth = require("./../middleware/checkAuth.js");
 
-router.get('/:nick', checkAuth, async function(req, res, next) {
-  try {
-    const [bleach, shinigami] = await Promise.all([
-      Bleach.findOne({nick: req.params.nick}),
-      Bleach.find({}, {_id: 0, title: 1, nick: 1})
-    ]);
-    if (!bleach) {
-      throw new Error("Нет такого Шинигами");
-    }
-    renderBleach(res, bleach.title, bleach.avatar, bleach.desc, shinigami);
-  } catch (err) {
-    next(err);
-  }
+router.get("/:nick", function(req, res, next) {
+  db.query(`SELECT * FROM bleaches WHERE bleaches.nick = '${req.params.nick}'`, (err, bleaches) => {
+  if(err) {
+    console.log(err);
+    if(err) return next(err)
+    } else {
+      if(bleaches.length == 0) return next(new Error("Нет такого Шинигами"))
+        var bleach = bleaches[0];
+        res.render('bleach', {
+          title: bleach.title,
+          picture: bleach.avatar,
+          desc: bleach.about
+        })
+      }
+  })
 });
 
-function renderBleach(res, title, picture, desc, shinigami) {
-  console.log(shinigami);
-  res.render('bleach', {
-    title: title,
-    picture: picture,
-    desc: desc,
-    // menu: shinigami,
-  });
-}
+
+// router.get('/:nick', checkAuth, async function(req, res, next) {
+//   try {
+//     const [bleach, shinigami] = await Promise.all([
+//       Bleach.findOne({nick: req.params.nick}),
+//       Bleach.find({}, {_id: 0, title: 1, nick: 1})
+//     ]);
+//     if (!bleach) {
+//       throw new Error("Нет такого Шинигами");
+//     }
+//     renderBleach(res, bleach.title, bleach.avatar, bleach.desc, shinigami);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// function renderBleach(res, title, picture, desc, shinigami) {
+//   console.log(shinigami);
+//   res.render('bleach', {
+//     title: title,
+//     picture: picture,
+//     desc: desc,
+//     // menu: shinigami,
+//   });
+// }
 
 module.exports = router;
