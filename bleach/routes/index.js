004f378267d3b951440cfe63e239170c50cdbd5f
kageyoshi <diana.azamatovva@gmail.com>
2024-01-12 21:13:53 +0300

    11.6 создание таблицы user

--- bleach/routes/index.js
+++ bleach/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Bleach = require("../models/bleach").Bleach;
-const User = require("../models/user").User;
+var db = require('../mySQLConnect.js');
+// const Bleach = require("../models/bleach").Bleach;
+// const User = require("../models/user").User;
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
+    req.session.greeting = "Hi!!!";
     res.render('index', { title: 'Bleach', counter:req.session.counter });
   } 
   catch (err) {
@@ -22,26 +24,49 @@ router.get('/logreg', async function(req, res, next) {
 router.post('/logreg', async function(req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
-  try {
-      const user = await User.findOne({ username });
-
-      if (user) {
-          if (user.checkPassword(password)) {
-              req.session.user = user._id;
-              res.redirect('/');
-          } else {
-              res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
-          }
+  db.query(`SELECT * FROM user WHERE user.username = '${req.body.username}'`, function(err, users) {
+    if (err) return next(err);
+    if (users.length > 0) {
+      var user = users[0];
+      if (password == user.password) {
+        req.session.user = user.id;
+        res.redirect('/');
       } else {
-          const newUser = new User({ username, password });
-          await newUser.save();
-          req.session.user = newUser._id;
-          res.redirect('/');
+        // Неправильный пароль
+        res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
       }
-  } catch (err) {
-      next(err);
-  }
+    } else {
+      db.query(`INSERT INTO user (username, password) VALUES ('${username}', '${password}')`, function(err, user) {
+        if (err) return next(err);
+        req.session.user = user.id;
+        res.redirect('/');
+      });
+    }
+  });
 });
+
+
+
+//   try {
+//       const user = await User.findOne({ username });
+
+//       if (user) {
+//           if (user.checkPassword(password)) {
+//               req.session.user = user._id;
+//               res.redirect('/');
+//           } else {
+//               res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
+//           }
+//       } else {
+//           const newUser = new User({ username, password });
+//           await newUser.save();
+//           req.session.user = newUser._id;
+//           res.redirect('/');
+//       }
+//   } catch (err) {
+//       next(err);
+//   }
+// });
 
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{error:null});
@@ -55,5 +80,3 @@ router.post('/logout', function(req, res, next) {
 });
 
 module.exports = router;
-
-
