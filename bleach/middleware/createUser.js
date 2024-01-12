d2deaff86959057c566337c22fa0fa0e3b44fb84
kageyoshi <diana.azamatovva@gmail.com>
2024-01-12 21:24:01 +0300

    11.7 информация о пользователе

--- bleach/middleware/createUser.js
+++ bleach/middleware/createUser.js
@@ -1,12 +1,17 @@
+var db = require('../mySQLConnect.js');
 module.exports = function(req,res,next) {
-res.locals.user = null
-// User.findById(req.session.user, function (err, user) { // if (err)
-// return next(err)
-// res.locals.user = user;
-// next();
-// })
-next();
-}
+  res.locals.user = null;
+  console.log('Проверяем сессию');
+  console.log(req.session.user);
+  db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err, users) => {
+  if(err)
+    return next(err);
+  if(users.length > 0) {
+    res.locals.user = users[0];
+  };
+  next();
+  });
+};
 
 // const User = require("../models/user").User;
 
